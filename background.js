5f6788f3be22a4e3d4d45f8063303ede1b798840
qufighter <qufighter@gmail.com>
2012-10-02 01:23:51 -0400

    version 2 manifest.json changes

---
+++ background.js
@@ -0,0 +1,259 @@
+var maxHistory = 15;//PerWindow
+var dothumbs = false;
+var selWindows=[],selwIdx=[],tabImgs=[];
+var thscale=0.09;
+var hqthumbs=false;
+var onewin=false;
+var justback=false;
+var thumbwidth=100;
+var thHeiRatio=0.75;
+var currentWindow = 1;//to track which set of tabs to return to the popup.html
+var tabsWindows=[];
+function fromPrefs(){
+	if(localStorage["maxhistory"]-0 > 0 ) maxHistory = new Number(localStorage["maxhistory"]) + 1;
+	dothumbs = ((localStorage["dothumbs"]=='true')?true:false);
+	if(dothumbs==false)tabImgs=[];
+	hqthumbs = ((localStorage["hqthumbs"]=='true')?true:false);
+	onewin = ((localStorage["onewin"]=='true')?true:false);
+	justback = ((localStorage["justback"]=='true')?true:false);
+	if(justback){
+		chrome.browserAction.setIcon({path:'icon19back.png'});
+	}else{
+		chrome.browserAction.setIcon({path:'icon19.png'});
+	}
+	if(onewin){
+		currentWindow=1;
+		//merge all history somehow....??
+	}else{
+		tabsWindows=[];
+	}
+	if(hqthumbs){
+		thumbwidth=200;
+		//var tim=pim;
+		//tim.width=200,tim.height=150;
+		//tim = example;
+		//tim.width=200,tim.height=150;
+		thscale=0.18;
+	}else{
+		thumbwidth=100;
+		//var tim=pim;
+		//tim.width=150,tim.height=75;
+		//tim = example;
+		//tim.width=150,tim.height=75;
+		thscale=0.09;
+	}
+}
+
+function clearAll(dup,arr,winid){
+	var narr=[],l=arr.length-1;
+	for(i=0;i<l;i++){if(arr[i]!=dup)narr.push(arr[i]);}
+	selwIdx[winid]=narr.length-1;
+	return narr;
+}
+
+function clearDups(dup,arr,winid){
+	var narr=clearAll(dup,arr,winid);
+	narr.push(dup);
+	selwIdx[winid]=narr.length-1;
+	return narr;
+}
+
+var iconimg=new Image();
+iconimg.src="icon19.png";
+
+function updateIcon(windowId){
+	if(!dothumbs)return;
+	var curIdx = selwIdx[windowId];
+	if( !selWindows[windowId][curIdx-1] || !tabImgs[selWindows[windowId][curIdx-1]] ){
+		//console.log(windowId+'nol'+curIdx+' '+selWindows[windowId][curIdx+1]);
+		return;
+	}
+	chrome.tabs.get(selWindows[windowId][curIdx-1], function(tab){
+		var icvs = document.createElement('canvas');//icon canvas
+		var sx,sy;
+		var totalWidth = 19;
+		icvs.width=totalWidth
+		icvs.height=totalWidth
+		var ictx = icvs.getContext("2d");
+		favimg=new Image();
+		favimg.onload = function(){
+	    ictx.drawImage(iconimg,0,0);
+	    //ictx.scale(0.9,0.9);
+			ictx.drawImage(favimg,3,2);
+			chrome.browserAction.setIcon({imageData:ictx.getImageData(0, 0, 19, 19)});//update icon (to be configurable)
+	  	icvs=null;
+	  }
+		favimg.src=tabImgs[tab.id];//tab.favIconUrl;
+		});
+}
+function historyAdd(winId,tabId){
+	if( !selWindows[winId] ){
+		selWindows[winId] = new Array();
+		selwIdx[winId]=new Number(-1);
+	}
+	//if( selWindows[winId][selwIdx[winId]] == tabId ) return; //same as one on top already
+	
+	selWindows[winId].push(tabId);
+	selWindows[winId]=clearDups(tabId,selWindows[winId],winId);//selwIdx[winId]+=1; //handles increment as well
+	if(selWindows[winId].length >maxHistory){
+		//console.log("history exceeded"+ selWindows[winId]);
+		selWindows[winId]=selWindows[winId].splice(1,maxHistory);
+		//console.log("history exceeded"+ selWindows[winId]);
+		selwIdx[winId]-=1;
+	}
+	if(dothumbs)captureImage(winId,tabId);
+	//console.log(selwIdx[winId]+' win:'+winId +' tab:'+tabId);
+	chrome.browserAction.setTitle({title:(selWindows[winId].length-1)+' LASTabZ'})
+	updateIcon(winId);
+}
+function setCurrentWindow(tabId,selectInfo){
+	if(!onewin){
+		currentWindow=selectInfo.windowId;
+	}else{
+		tabsWindows[tabId]=selectInfo.windowId;//no longe rused?
+	}
+	historyAdd(currentWindow,tabId);
+}
+chrome.tabs.onSelectionChanged.addListener(setCurrentWindow);
+chrome.windows.onFocusChanged.addListener(function(windowId){
+	chrome.tabs.getSelected(windowId,function(tab){
+		setCurrentWindow(tab.id,{windowId:tab.windowId})
+	})
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
+	if(dothumbs)captureImage(tab.windowId,tabId);//while user navigates optionally capture screenshot updates
+});
+
+function removedTab(closedTab) {
+	disablelasttab = ((localStorage["disablelasttab"]=='true')?true:false);
+	//BUG - a popup window for some reason counts in the tab list for a window
+	var clwin=currentWindow;
+	if( !disablelasttab && closedTab == selWindows[currentWindow][selwIdx[currentWindow]] && selWindows[currentWindow].length > 1 ){
+		var finalTarget = selWindows[currentWindow][selwIdx[currentWindow]-1];
+		selWindows[currentWindow]=clearAll(closedTab,selWindows[currentWindow],currentWindow);
+		setTimeout(function(){//unnecessary timeout?
+			chrome.windows.getCurrent(function(window){//could possibly use shortcut here - currentWindow should be up to date, however this delay is necessary since the window is switched after the tab is closed, we have to make sure that occurs first and registers in the history array?
+				var windowId = onewin?currentWindow:window.id;//currentWindow
+				if( clwin==windowId ){//ohterwise they just closed a window
+					var curIdx = selwIdx[windowId];
+					if( selWindows[windowId][(curIdx)]!= finalTarget ){ //selWindows[windowId][(curIdx-1)]==closedTab  && finalTarget==selWindows[windowId][(curIdx-2)]  && finalTarget!=selWindows[windowId][(curIdx)] && selWindows[windowId].length > 2){//we have enough history, and we were viewing the closed tab
+						var nextTab = selWindows[windowId][(curIdx-1)];// FINALtARGET
+						//trim the items that are extra (the accidental select and the removed tab)
+						selWindows[windowId]=selWindows[windowId].splice(0,(curIdx));selwIdx[windowId]-=1;//REMOVE FAULTY ENTRY
+						//chrome.windows.update(tabsWindows[nextTab],{top:0,left:0}, function(w){
+						
+							chrome.tabs.update(nextTab,{selected:true},function(a){//SELET CORRECT TAB
+							//console.log('last tab now selected')
+							})
+						
+						//})
+					}
+				}
+			})
+		},3);
+	}else{
+		selWindows[currentWindow]=clearAll(closedTab,selWindows[currentWindow],currentWindow);
+	}
+	if(tabImgs[closedTab]){
+		//tabImgs[closedTab]=false;
+		tabImgs[closedTab]=null;
+		cleanupEmptyImages();
+	}
+	updateIcon(currentWindow)
+}
+
+chrome.tabs.onRemoved.addListener(removedTab);
+
+chrome.windows.onRemoved.addListener(function(winId) {
+	if( selWindows[winId] ){
+		//selWindows[winId] = false;
+		//selwIdx[winId]=0;
+		selWindows[winId]=null;
+		selwIdx[winId]=null;
+		cleanupEmptyWindows()
+	}
+});
+
+//Warning i you ever sort tabs based on window, references to which window thumb belongs to change here! OR else risk duplicate tracking
+chrome.tabs.onDetached.addListener(removedTab);
+//		function( tabId, d) {
+//			selWindows[d.oldWindowId]=clearAll(tabId,selWindows[d.oldWindowId],d.oldWindowId);
+//			cleanupEmptyWindows()
+//		});
+
+function cleanupEmptyWindows(){
+	var nsw = new Array();
+	for(i in selWindows){
+		if( typeof(selWindows[i]) == 'object' && selWindows[i]!=null && selWindows[i].length > 0){
+			nsw[i]=selWindows[i];
+		}
+	}
+	selWindows=nsw;
+}
+
+function cleanupEmptyImages(){
+	var nsw = new Array();
+	for(i in tabImgs){
+		if( typeof(tabImgs[i]) == 'string' && tabImgs[i]!=null && tabImgs[i].length > 0){
+			nsw[i]=tabImgs[i];
+		}
+	}
+	tabImgs=nsw;
+}
+
+chrome.extension.onRequest.addListener(
+function(request, sender, sendResponse){
+    if (request.greeting == "gettabs" && selWindows[currentWindow]){
+      sendResponse({farewell: selWindows[currentWindow]});
+    }else if(request.greeting == "lastab"){
+    	chrome.tabs.update(selWindows[currentWindow][selwIdx[currentWindow]-1],{selected:true},function(){/*changed tab*/})
+    	sendResponse({});
+    }else if(request.greeting == "gettabimg"){
+    	if(tabImgs[request.tabId]){
+    		sendResponse({scr: tabImgs[request.tabId]});
+    	}else{
+    		sendResponse({});
+    	}
+    }else if(request.greeting == "getallwindowshistory"){
+      sendResponse({farewell:selWindows});//mode not
+    }else if(request.greeting == "reloadprefs"){
+    	fromPrefs();sendResponse({});
+    }else
+    	sendResponse({});
+  
+});
+function captureImage(winId,tabId){
+	chrome.tabs.captureVisibleTab(winId, function(dataUrl){
+		pim.src=dataUrl;
+		window.setTimeout(function(){
+			var img = pim;
+			img.width = thumbwidth;
+			img.height = thumbwidth*thHeiRatio;
+	  	var cvs = example;
+	  	cvs.width = thumbwidth;
+			cvs.height = thumbwidth*thHeiRatio;
+	  	var ctx = cvs.getContext("2d")
+	  	ctx.clearRect(0,0,img.width,img.height);
+	  	ctx.putImageData(getImageDataFromImage(img), 0, 0);
+	  	tabImgs[tabId]=cvs.toDataURL("image/jpeg",0.3);
+			pim.src='';
+		},255);
+	});
+}
+function getImageDataFromImage(idOrElement){
+	var theImg = (typeof(idOrElement)=='string')? document.getElementById(idOrElement):idOrElement;
+	var cvs = document.createElement('canvas');
+	cvs.width = thumbwidth;
+	cvs.height = thumbwidth*thHeiRatio;
+	var ctx = cvs.getContext("2d");
+	ctx.scale(thscale,thscale);
+	ctx.drawImage(theImg,0,0);
+	var id=ctx.getImageData(0, 0, cvs.width, cvs.height);
+	cvs=null;
+	return (id);
+}
+
+var pim = document.createElement('img');
+var example = document.createElement('canvas');
+fromPrefs()
---
+++ popup.js
@@ -0,0 +1,380 @@
+var pressedTabs=[],ecurTab=0,closeMode=false;
+function getEventTargetA(ev){
+	ev = ev || event;
+	var targ=(typeof(ev.target)!='undefined') ? ev.target : ev.srcElement;
+	if(targ !=null){
+	    if(targ.nodeType==3)
+	        targ=targ.parentNode;
+	}
+	if(targ.nodeName != 'A')return targ.parentNode;
+	return targ;
+}
+function addBorder(who){
+	who.previousSibling.style.border='1px solid red';
+	who.style.border="1px solid red";
+}
+function remBorder(who){
+	who.previousSibling.style.border='none';
+	who.style.border='';
+}
+function hasBorder(who){if(who.style.border=="1px solid red")return true;return false;}
+function queueClose(who){pressedTabs[who.name]=(who);}
+function unqueueClose(who){pressedTabs[who.name]=false;}
+function clearPressed(){
+	for(var t in pressedTabs){
+		if(pressedTabs[t]) remBorder(pressedTabs[t]);
+	}
+	pressedTabs=[];
+}
+function pressTab(ev,who,isfirst){
+	who=getEventTargetA(ev);
+	if(who.parentNode.className!='thinrow' && ev.button==1){
+		clearPressed()
+		closeMode=true;
+		addBorder(who);
+		queueClose(who);
+		ecurTab=who;
+	}
+}
+function relesTab(ev){
+	who=getEventTargetA(ev);
+	if(ev.button==1){
+		remTabs(who);
+	}
+}
+function mouseOverTab(ev,who,isfirst){
+	who=getEventTargetA(ev);
+	if(who.parentNode.className!='thinrow' && closeMode && ev.button==1 && ecurTab!=who){
+		if(hasBorder(who)){
+			remBorder(who);
+			unqueueClose(who);
+		}else{
+			addBorder(who);
+			queueClose(who);
+		}
+		ecurTab=who;
+	}
+
+	pr(who)
+//  		if(ev.button==1){
+//  			if(isfirst || pressedTab==who){
+//  				who.previousSibling.style.border='1px solid red';
+//  				who.style.border="1px solid red";
+//  				pressedTab=who
+//				}
+//			}
+}
+function mouseOutTab(ev,who){
+	who=getEventTargetA(ev);
+//  		if(ev.button==1){
+//  			who.previousSibling.style.border='none';
+//  			who.style.border='';
+//  		}
+}
+
+function switchToTab(ev,who){
+	who=getEventTargetA(ev);
+	if(ev.button==1){remTab(who);return;};if(who.name=='LOAD_HIST'){who.childNodes[1].innerText='';loadRest(true);return;};if(who.name=='LOAD_MORE'){who.childNodes[1].innerText='';loadAllTabs();return;};if(who.name=='LOAD_DNS'){who.childNodes[1].innerText='';loadAllTabs(false,false,true);return;};if(who.name=='LOAD_ALPHA'){who.childNodes[1].innerText='';loadAllTabs(false,true);return;};if(who.name=='LOAD_DEFAULT'){who.childNodes[1].innerText='';loadAllTabs(true);return;};document.body.style.marginRight='auto';document.body.addEventListener('mousedown',function(){window.close},true);
+	chrome.tabs.update(who.name-0,{active:true},function(){/*changed tab*/})
+}
+function remTabs(who){if(hasBorder(who)){for(var t in pressedTabs){if(pressedTabs[t]) remTab(pressedTabs[t]);}pressedTabs=[];}else{clearPressed();remTab(who);}}
+function remTab(who){who.parentNode.parentNode.removeChild(who.parentNode);chrome.tabs.remove(who.name-0,function(){/*changed tab*/})}
+var tabsGotten=[],tabsLoaded=[],curTab=0;
+var lockPr=false,curpr=0,hasAdd=false;
+var doThumbs = ((localStorage["dothumbs"]=='true')?true:false);
+var showFirstItem = ((localStorage["showCurrentTab"]=='true')?true:false);
+var justback = ((localStorage["justback"]=='true')?true:false);
+if(justback){
+	chrome.extension.sendRequest({greeting: "lastab"}, function(response) {})
+	window.close();
+	return;
+}
+var offstringcut = 21;//truncate titles will be ocfigurable as wol wildth
+function pr(who){};
+if(doThumbs){
+	function pr(who){
+		//window.setTimeout(function(){
+  		if(!lockPr && curpr!=who){
+  			pre(who);
+  			lockPr=true;
+  		}
+  		curpr=who;
+  		window.setTimeout(function(){lockPr=false;if(curpr==who)pre(who);},50);
+  	//},100);
+	}
+	function getOffset( el ){
+		  var _x=0,_y=0;
+	    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+	    		_x+=el.offsetLeft;// - el.scrollLeft;
+	    		_y+=el.offsetTop;// - el.scrollTop;  
+	        el = el.offsetParent;
+	    }return { y: _y, x: _x };
+	}
+	function pre(who){
+		chrome.extension.sendRequest({greeting: "gettabimg",tabId:(who.name-0)}, function(response) {
+			if(response.scr){
+				if(! document.getElementById('pim')){
+					var p=document.createElement('img');
+					p.src=response.scr;p.id='pim';p.width=150;p.className='prvw'
+					document.body.appendChild(p);
+				}else{
+					window.setTimeout(function(){document.getElementById('pim').src=response.scr;document.getElementById('pim').style.display=(response.scr.length > 0?'block':'none');},10);
+					var offs=getOffset(who);
+					if(offs.y+112>document.body.clientHeight){
+						offs.y=document.body.clientHeight-112;
+						if(offs.y<0)offs.y=0;
+					}
+					document.getElementById('pim').style.top=offs.y;
+				}
+			}else{
+				if(document.getElementById('pim')){
+					document.getElementById('pim').src='';
+					document.getElementById('pim').style.display='none';
+				}
+			}
+			//lockPr=false;
+		});
+		//},10);
+	}
+}
+function dovents(n){
+	n.addEventListener('mousedown', pressTab);
+  n.addEventListener('mouseup', relesTab);
+  n.addEventListener('mouseover', mouseOverTab);
+  n.addEventListener('mouseout', mouseOutTab);
+  n.addEventListener('click', switchToTab);
+}
+
+function maekTab(tab,b){
+	if(curTab>=15){
+		document.body.style.overflowY='auto';
+		document.body.style.marginRight='16px';
+	}
+	var n=document.getElementById('f1').cloneNode(true);
+	n.id='';
+	n.firstChild.title='Close \n'+tab.url;
+	n.firstChild.name=tab.id;
+	n.childNodes[1].className=(tab.selected?"sel":"");
+	n.childNodes[1].name=tab.id;
+	var turl=tab.url;
+	if(turl.length > 256)turl=turl.substr(0,128)+'... ...'+turl.substr(turl.length-128);
+	n.childNodes[1].title=tab.title+' \n\n'+turl;
+	n.childNodes[1].childNodes[0].src=tab.favIconUrl;
+	n.childNodes[1].childNodes[1].innerText='\u00a0'+tab.title.replace('http://','').replace('www.','').substr(0,offstringcut);//fars vor
+	//if(b)document.body.insertBefore(n,document.getElementById('LOAD_MORE'));
+	document.body.appendChild(n);
+	dovents(n);
+	tabsLoaded[tab.id]=true;
+	//curTab++;//keep counting //jackass // why did you hide this in a function // why are you looking at this // it belongs here
+	//return;//dont return until now
+}
+function clearAllReset(){
+	tabsLoaded=[];
+	var n=document.getElementById('f1').cloneNode(true);
+	document.body.innerHTML='';
+	document.body.appendChild(n);
+	curTab=0;
+}
+ //think about it fool cuz its a joke that tabs could change while this is open?? or no....?? search getAllInWindow 2x= no 
+//if(who.name=='LOAD_ALPHA'){who.childNodes[1].innerText='';loadAlphabetical();return;};if(who.name=='LOAD_DEFAULT'){who.childNodes[1].innerText='';loadAllTabs(true);return;};
+function loadAllTabs(defaultOrdering,alphaOrdering,urlOrdering){
+	if(typeof(defaultOrdering)=='undefined')defaultOrdering=false;
+	if(typeof(alphaOrdering)=='undefined')alphaOrdering=false;
+	if(typeof(urlOrdering)=='undefined')urlOrdering=false;
+
+	
+	
+	//chrome.tabs.getAllInWindow(null, function(tabs) {
+		var tabs=allInWindow;
+		var cdn=function(i,l){return i>-1;}
+		var inc=-1;
+		var l=tabs.length;
+		var i=l-1;
+		
+		if(defaultOrdering||alphaOrdering||urlOrdering){
+  		clearAllReset();
+  		cdn=function(i,l){return i<l;}
+  		inc=1,i=0;
+  	}
+  	
+  	if(alphaOrdering){
+  		tabs.sort(function(a, b){
+			 var nameA=a.title.toLowerCase(), nameB=b.title.toLowerCase()
+			 if (nameA < nameB) //sort string ascending
+			  return -1 
+			 if (nameA > nameB)
+			  return 1
+			 return 0 //default return value (no sorting)
+			})
+  	}else if(urlOrdering){
+  		tabs.sort(function(a, b){
+  			//.replace('http://','').replace('https://','')
+			 var nameA=a.url.toLowerCase(), nameB=b.url.toLowerCase()
+			 if (nameA < nameB) //sort string ascending
+			  return -1 
+			 if (nameA > nameB)
+			  return 1
+			 return 0 //default return value (no sorting)
+			})
+  	}
+		
+		//for( i=tabs.length-1; cdn(i,l); i+=inc ){
+		for( ; cdn(i,l); i+=inc ){
+			var tab=tabs[i]
+			if(tab && !tabsLoaded[tab.id]){ 
+  			maekTab(tab);
+  		}curTab++;//stop
+		}
+		
+		//if(defaultOrdering||alphaOrdering)
+		addRemainingTabsLink(true);
+		
+		if(alphaOrdering||urlOrdering){//get sort back
+			chrome.tabs.getAllInWindow(null, function(t) {
+				allInWindow=t;
+			});
+		}
+	//});
+}
+var allInWindow=[];
+function loadRest(doReset){
+	if(typeof(doReset)=='undefined')doReset=false;
+	if(doReset)clearAllReset();
+	chrome.tabs.getAllInWindow(null, function(tabs){
+		allInWindow=tabs;
+		var allTabsnow=[];
+		for( var i=tabs.length-1; i>-1; i-- ){
+			allTabsnow[tabs[i].id]=tabs[i];
+		}
+		while(curTab<tabsGotten.length){
+			var tab=allTabsnow[tabsGotten[curTab]];
+			//console.log('--');
+			if(tab){
+  			maekTab(tab);
+  		}curTab++;//go
+  		//if(curTab > tabsGotten.length-2)window.setTimeout(addRemainingTabsLink,300);
+			//console.log(curTab, tabsGotten.length-2)
+		}
+		addRemainingTabsLink();
+	});
+}
+function createf1(){
+	var dcl='',moe='';
+	if(doThumbs){
+		document.body.style.marginLeft=150;
+		moe='onmouseover="pr(this)" ';
+		dcl='style="left:150px;"';
+	}
+}
+function cl(){
+	chrome.extension.sendRequest({greeting: "gettabs"}, function(response) {
+//document.body.innerHTML+=response.farewell;
+		if(response.farewell==undefined){addRemainingTabsLink();return;};
+		createf1();
+		var tabs = response.farewell;//ß.split(',');
+		tabsGotten=[],curTab=0,hasAdd=false;//,loadedTabs=0;
+		for( i=tabs.length-(showFirstItem?1:2); i >-1; i-- ){
+			if(tabs[i]-0 > 0 ){
+				tabsGotten.push(tabs[i]-0);
+			}
+		}
+		//if(tabsGotten.length>0)
+		loadnexttab();
+		//else addRemainingTabsLink();
+	});
+}
+function loadnexttab(){
+//			chrome.tabs.get(tabsGotten[curTab], function(tab) {
+//				if(tab){
+//					maekTab(tab);
+//				}
+//			});
+//			curTab++;
+//			if(curTab > 4 ){
+		loadRest();return;
+	//}
+	//if(curTab < tabsGotten.length )window.setTimeout(loadnexttab,14);
+}
+function addRemainingTabsLink(skipShowRemaining){
+	if(typeof(skipShowRemaining)=='undefined')skipShowRemaining=false;
+
+	//if(hasAdd)return;hasAdd=true;
+	//use maektab to do this, but no
+	if(document.getElementById('LOAD_MORE'))document.getElementById('LOAD_MORE').parentNode.removeChild(document.getElementById('LOAD_MORE'));
+	if(document.getElementById('LOAD_ALPHA'))document.getElementById('LOAD_ALPHA').parentNode.removeChild(document.getElementById('LOAD_ALPHA'));
+	if(document.getElementById('LOAD_DEFAULT'))document.getElementById('LOAD_DEFAULT').parentNode.removeChild(document.getElementById('LOAD_DEFAULT'));
+	if(document.getElementById('LOAD_DNS'))document.getElementById('LOAD_DNS').parentNode.removeChild(document.getElementById('LOAD_DNS'));
+
+	if(!skipShowRemaining){
+  	var n=document.getElementById('f1').cloneNode(true);
+		n.id='LOAD_MORE';
+		n.className="thinrow";
+		n.firstChild.style.display="none";
+		n.childNodes[1].className="";
+		n.childNodes[1].name="LOAD_MORE";
+		n.childNodes[1].title="Show non-history tabs for this window";
+		n.childNodes[1].childNodes[0].style.display="none";
+		n.childNodes[1].childNodes[1].innerText='\u00a0'+'Show Remaining Tabs...';
+		document.body.appendChild(n);
+		dovents(n);
+		//n.childNodes[1].addEventListener('mouseover',function(e){who=e.target;if(who.name=='LOAD_MORE')switchToTab(e,who)},true);
+  }else{
+  	var n=document.getElementById('f1').cloneNode(true);
+		n.id='LOAD_HIST';
+		n.className="thinrow";
+		n.firstChild.style.display="none";
+		n.childNodes[1].className="";
+		n.childNodes[1].name="LOAD_HIST";
+		n.childNodes[1].title="Shows historic tabs in order of their last viewing.  Tabs you were just in will be listed at the top.";
+		n.childNodes[1].childNodes[0].style.display="none";
+		n.childNodes[1].childNodes[1].innerText='\u00a0'+'Show History Order...';
+		document.body.appendChild(n);
+		dovents(n)
+  }
+	var n=document.getElementById('f1').cloneNode(true);
+	n.id='LOAD_ALPHA';
+	n.className="thinrow";
+	n.firstChild.style.display="none";
+	n.childNodes[1].className="";
+	n.childNodes[1].name="LOAD_ALPHA";
+	n.childNodes[1].title="Sort tabs alphabetically by tab title";
+	n.childNodes[1].childNodes[0].style.display="none";
+	n.childNodes[1].childNodes[1].innerText='\u00a0'+'Sort by Title...';
+	document.body.appendChild(n);
+	dovents(n)
+	
+	var n=document.getElementById('f1').cloneNode(true);
+	n.id='LOAD_DNS';
+	n.className="thinrow";
+	n.firstChild.style.display="none";
+	n.childNodes[1].className="";
+	n.childNodes[1].name="LOAD_DNS";
+	n.childNodes[1].title="Sort tabs alphabetically by their URL";
+	n.childNodes[1].childNodes[0].style.display="none";
+	n.childNodes[1].childNodes[1].innerText='\u00a0'+'Sort By Domain...';
+	document.body.appendChild(n);
+	dovents(n)
+	
+	var n=document.getElementById('f1').cloneNode(true);
+	n.id='LOAD_DEFAULT';
+	n.className="thinrow";
+	n.firstChild.style.display="none";
+	n.childNodes[1].className="";
+	n.childNodes[1].name="LOAD_DEFAULT";
+	n.childNodes[1].title="Show tabs in their default left to right order";
+	n.childNodes[1].childNodes[0].style.display="none";
+	n.childNodes[1].childNodes[1].innerText='\u00a0'+'Show Tab Order...';
+	document.body.appendChild(n);
+	dovents(n)
+	
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	cl();
+  document.getElementById('remTabs').addEventListener('click', remTabs);
+  document.getElementById('f1').addEventListener('mousedown', pressTab);
+  document.getElementById('f1').addEventListener('mouseup', relesTab);
+  document.getElementById('f1').addEventListener('mouseover', mouseOverTab);
+  document.getElementById('f1').addEventListener('mouseout', mouseOutTab);
+  document.getElementById('f1').addEventListener('click', switchToTab);
+});
